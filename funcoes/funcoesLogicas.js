54309257e8d7bad0843e1092c75a1d4540e6ba9f
luansier <luansrosa.arq@gmail.com>
2021-07-05 15:32:38 -0300

    refactoring index and logic function files

--- funcoes/funcoesLogicas.js
+++ funcoes/funcoesLogicas.js
@@ -1,4 +1,33 @@
-export const inverteMesDia = (mesDia) => {
+export const verificaChuvaOcorrendo= (chuva, data) => {
+    const anoAtual = data.getFullYear();
+  
+    const dataInicio = new Date(chuva.inicio + '/' + anoAtual);
+    const dataFim = new Date(chuva.fim + '/' + anoAtual);
+  
+    if ( dataInicio.getMonth() > dataFim.getMonth() ) {
+        const anoFinal = dataFim.getFullYear();
+        dataFim.setFullYear(anoFinal +1);
+    }
+  
+    return ( data >= dataInicio && data <= dataFim );
+  }
+  
+  export const verificaProximaChuva = (chuva, data) => {
+    const anoAtual = data.getFullYear();
+    const dataInicio = new Date(chuva.inicio + '/' + anoAtual);
+    const dataInicioLimite = new Date(data);
+  
+    if ( data.getMonth() > dataInicio.getMonth() ) {
+        const anoFinal = dataInicio.getFullYear();
+        dataInicio.setFullYear(anoFinal +1);
+    }
+  
+    dataInicioLimite.setMonth( dataInicioLimite.getMonth() + 2 );
+  
+    return ( data < dataInicio && dataInicio < dataInicioLimite );
+  }
+  
+  export const inverteMesDia = (mesDia) => {
     const dataInvertida = mesDia.split('/');
   
     return dataInvertida[1] + '/' + dataInvertida[0];
