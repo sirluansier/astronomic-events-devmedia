7a0d1b42c09a89cfc5e4567c479be8e769b754f9
luansier <luansrosa.arq@gmail.com>
2021-07-05 15:28:40 -0300

    new file created and function file refactored

--- funcoes/funcoesInterface.js
+++ funcoes/funcoesInterface.js
@@ -1,10 +1,23 @@
+import { inverteMesDia, retornaHemisferio, retornaIntensidade } from "./funcoesLogicas.js";
+
 const imprimeListaChuva = (listaChuvas) => {
-    listaChuvas.forEach( imprimeItemListaChuva );
-  }
-  
-  const imprimeItemListaChuva = (chuva) => {
-    console.log(`${ chuva.nome } - ${ chuva.intensidade } - ${ chuva.declinacao } - ${ chuva.inicio } à ${ chuva.fim } `);
-  }
-  
-  export default imprimeListaChuva;
-  
+  console.log('\nNOME DO METEORO             - INTENSIDADE - HEMISFÉRIO - PERÍODO');
+
+  listaChuvas.forEach( imprimeChuva );
+}
+
+const imprimeChuva = (chuva) => {
+  const nome = chuva.nome.padEnd(27,' ');
+  let intensidade = retornaIntensidade(chuva.intensidade);
+  let hemisferio = retornaHemisferio(chuva.declinacao);
+
+  intensidade = intensidade.padEnd(11, ' ');
+  hemisferio = hemisferio.padEnd(10,' ');
+
+  const dataInicio = inverteMesDia(chuva.inicio);
+  const dataFim = inverteMesDia(chuva.fim);
+
+  console.log(`${ nome } - ${ intensidade } - ${ hemisferio } - ${ dataInicio } à ${ dataFim } `);
+}
+
+export default imprimeListaChuva;
---
+++ funcoes/funcoesLogicas.js
@@ -0,0 +1,22 @@
+export const inverteMesDia = (mesDia) => {
+    const dataInvertida = mesDia.split('/');
+  
+    return dataInvertida[1] + '/' + dataInvertida[0];
+  }
+  
+  export const retornaIntensidade = (intensidade) => {
+    let novaIntensidade = '1 (Fraca)';
+  
+    if ( intensidade.indexOf('Forte') >= 0 ) {
+        novaIntensidade = '3 (Forte)';
+    } else if ( intensidade.indexOf('Média') >= 0 ) {
+        novaIntensidade = '2 (Média)';
+    } else if (  intensidade.indexOf('Irregular') >= 0 ) {
+        novaIntensidade = '(Irregular)';
+    }
+  
+    return novaIntensidade;
+  }
+  
+  export const retornaHemisferio = (declinacao) => declinacao >= 0 ? 'Norte' : 'Sul';
+  
